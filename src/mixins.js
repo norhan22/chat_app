22d86662d7ec1ae177066a31e11907e81b25affe
norhan <nora.moh.2011@gmail.com>
2021-05-23 04:41:37 +0200

    added mixin to the app

---
+++ src/mixins.js
@@ -0,0 +1,19 @@
+const
+  Joi = require('joi'),
+
+  mixins = {
+    checkValidation : (data) => {
+      const schema = Joi.object({
+          content: Joi.string().min(3).required()
+          // sender_id: Joi.number().required(),
+          // receiver_id:Joi.number().required()
+        }),
+        {error} = schema.validate(data)
+
+      if (error) return (error.details.map(d => d.message)).join(',')
+      else return true
+    }
+    
+  }
+module.exports = mixins
+ 

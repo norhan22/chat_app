31a9b670708246eb45bbf5c03e15fbeb748a407c
norhan <nora.moh.2011@gmail.com>
2021-07-11 02:55:55 +0200

    added chat class with chat methods

---
+++ public_html/scripts/chat.js
@@ -0,0 +1,98 @@
+
+class BasicRules {
+  constructor () {
+
+  }
+
+  /////////////////////////////////
+  // HTTP REQUEST
+  ////////////////////////////////
+  httpRequest (method, link, data) {
+    return new Promise((resolve, reject) => {
+      const xhttp = new XMLHttpRequest()
+
+      xhttp.open(method, link, true)
+      xhttp.setRequestHeader('Content-type', 'application/json')
+      xhttp.send(JSON.stringify(data))
+
+      xhttp.onload = function () {
+        if (this.status == 200) resolve(JSON.parse(xhttp.response))
+        else reject(xhttp.response)
+      }
+
+    })
+
+
+  }
+}
+class Chat extends BasicRules {
+  constructor () {
+    super() // call the constructor of the parent class
+
+    this.userID = null
+    this.activeUser = null
+    this.contacts = []
+
+  }
+
+  //////////////////////////
+  //  Handle Errors 
+  //////////////////////////
+  errorMsg (err, errFrom = '') {
+    console.error(`${errFrom || ''}`, err)
+
+  }
+
+  //////////////////////////
+  //  Contacts 
+  //////////////////////////
+
+  submitUser (userName, endpoint) {
+    return new Promise((resolve, reject) => {
+
+      const payload = {}
+      payload.name = userName
+      this.httpRequest('POST', endpoint, payload)
+        .then(res => {
+          console.log('addContact res', res)
+          this.activeUser = res
+          payload.id = res.id
+          resolve(payload)
+
+        })
+        .catch((err) => {
+          this.errorMsg(err, 'submitUser')
+          reject(err)
+        })
+    })
+  }
+  getContacts () {
+    this.httpRequest('GET', `${this.apiResource}/contacts`)
+      .then(res => {
+        this.contacts = res
+      })
+  }
+
+
+  //////////////////////////
+  //  Message
+  //////////////////////////
+
+  submitMsg (payload, endPoint) {
+    return new Promise((resolve, reject) => {
+      
+      this.httpRequest('POST', endPoint, payload)
+        .then((res) => {
+          resolve(res)
+        })
+        .catch((err) => {
+          this.errorMsg(err, 'submitMsg')
+          reject(err)
+        })
+
+    })
+
+  }
+
+
+}
---
+++ public_html/scripts/script.js
@@ -0,0 +1,131 @@
+const
+  socketIo = io(),
+  apiResource = 'http://localhost:8081/api/chat',
+  addContactEndpoint = `${apiResource}/addContact`,
+  sendMsgEndpoint = `${apiResource}//send-message`,
+  getContactsEdpoint = `${apiResource}/contacts`,
+
+
+  $ = document.querySelector.bind(document),
+  $$ = document.querySelectorAll.bind(document),
+
+  // MSG
+  chatForm = $('#chat'),
+  msgContent = chatForm.querySelector('input'),
+  messagesDiv = $('#messages'),
+
+  // Sign up
+  signUpForm = $('#signUp'),
+  User = signUpForm.querySelector('input').value,
+  activeUser = {}
+
+
+const chat = new Chat()
+
+//////////////////////////
+//  UI 
+//////////////////////////
+function welcome () {
+  const welcome = document.createElement('h4')
+  welcome.textContent = `welcome ${activeUser.name}`
+  messagesDiv.appendChild(welcome)
+
+}
+
+function showChat () {
+  chatForm.classList.remove('hide')
+  chatForm.classList.add('show')
+
+  signUpForm.classList.remove('show')
+  signUpForm.classList.add('hide')
+}
+
+function updateMessages (data) {
+  console.log(data)
+  const
+    msg = data.content,
+    messageEl = document.createElement('p'),
+    isSent = data.senderId === activeUser.id
+
+  if (isSent) messageEl.classList.add('sent')
+
+  messageEl.innerText = `${data.senderName} : ${msg}`
+  messagesDiv.appendChild(messageEl)
+}
+
+/////////////////////////////////
+// Socket IO 
+////////////////////////////////
+// Emit 
+function emitSocket (name, payload) {
+  socketIo.emit(name, payload)
+}
+
+// Listen 
+function listenSocket (name, callback) {
+  socketIo.on(name, callback)
+}
+// Listeners 
+(function () {
+  listenSocket('newUser', (user) => alert(`${user.name} is registered`))
+})();
+
+
+(function () {
+  listenSocket('newMsg', (msg) => updateMessages(msg))
+})()
+
+//////////////////////////
+//  Handle Errors 
+//////////////////////////
+function errorMsg (err, errFrom = '') {
+  $('#error').innerText = err
+  console.error(`${errFrom || ''}`, err)
+}
+
+function resetErrMsg () {
+  $('#error').innerTex = ''
+}
+///////////////////////////////
+
+// Register Contact
+signUpForm.addEventListener('submit',
+  function (e) {
+    e.preventDefault()
+    if (User) { 
+      chat.submitUser(User, addContactEndpoint)
+        .then((res) => {
+          emitSocket('newUser', res)
+          resetErrMsg()
+          activeUser.name = res.name
+          activeUser.id = res.id
+          
+          welcome()
+          showChat()
+        }).catch(err => errorMsg(err))
+    }
+    return false
+  })
+
+// Submit MSG
+function sendMsg () {
+  const payload = {
+    content: msgContent.value,
+    senderId : activeUser.id
+  }
+  chat.submitMsg(payload, sendMsgEndpoint)
+    .then((res) => {
+      resetErrMsg()
+      updateMessages(res)
+      emitSocket('newMsg', res)
+    })
+}
+chatForm.addEventListener('submit',
+  function (e) {
+    e.preventDefault()
+    if (msgContent.value) sendMsg()
+    else errorMsg('please enter a message')
+  
+    return false
+
+  })
